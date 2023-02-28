22c7b63245e796a77251b86cb67a72c68a81fb70
amgg16 <124928492+amgg16@users.noreply.github.com>
2023-02-28 00:16:42 -0500

    :100:

--- televisores/tv.py
+++ televisores/tv.py
@@ -33,14 +33,14 @@ class TV:
         return self._volumen
     
     def setVolumen(self, volumen):
-        if self._estado==True and volumen>=0 and volumen <=120:  
+        if ((self._estado==True) and (volumen>=0 and volumen <=7)):  
             self._volumen=volumen
 
     def getCanal(self):
         return self._canal
     
     def setCanal(self, canal):
-        if self._estado==True and canal>=0 and  canal<=120:  
+        if ((self._estado==True) and (canal>=1 and  canal<=120)):  
             self._canal=canal
 
     @classmethod
@@ -65,14 +65,14 @@ class TV:
             self._canal+=1
     
     def canalDown(self,canal):
-        if self.estado==True and canal>1 and canal<120:
+        if self._estado==True and canal>1 and canal<120:
             self._canal-=1
 
-    def volumenlUp(self,volumen):
-        if self.estado==True and volumen>0 and volumen<7:
+    def volumenUp(self,volumen):
+        if self._estado==True and volumen>0 and volumen<7:
             self._volumen+=1
 
     def volumenDown(self,volumen):
-        if self.estado==True and volumen>0 and volumen<7:
+        if self._estado==True and volumen>0 and volumen<7:
             self._volumen-=1
 
